c11dc55f5daf88ad0145076b834a9dbba0dd09ee
Barbara <barbara.strapac@gmail.com>
2019-04-10 14:01:39 +0200

    -effect jquery
    -needs smoother animation

--- assets/js/mobile.js
+++ assets/js/mobile.js
@@ -1,9 +1,44 @@
 
 $('.slider').on('swiperight', function(){
-	$( ".slider_up li:first" ).appendTo( ".slider_up");
-	$( ".slider_down li:first" ).appendTo( ".slider_down");
+	var image_up = $( ".slider_up li:first" );
+	var image_down = $( ".slider_down li:first" );
+	var image_width= image_up.width();
+	$('.slider_up').css({
+		position: 'relative'
+	}).animate(
+		{
+			right: 0
+		}, "slow"
+	);
+	$('.slider_down').css({
+		position: 'relative',
+		right: item_width
+	}).animate(
+		{
+			right: 0
+		}, "slow"
+	);
+	image_up.detach().appendTo( ".slider_up");
+	image_down.detach().appendTo( ".slider_down");
 });
 $('.slider').on('swipeleft', function(){
-	$( ".slider_up li:last" ).prependTo( ".slider_up");
-	$( ".slider_down li:last" ).prependTo( ".slider_down");
+	var image_up = $( ".slider_up li:last" );
+	var image_down = $( ".slider_down li:last" );
+	var image_width= image_up.width();
+	$('.slider_up').css({
+		position: 'relative'
+	}).animate(
+		{
+			left: image_width
+		}, "slow"
+	);
+	$('.slider_down').css({
+		position: 'relative'
+	}).animate(
+		{
+			left: image_width
+		}, "slow"
+	);
+	image_up.detach().prependTo( ".slider_up");
+	image_down.detach().prependTo( ".slider_down");
 });
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,48 @@
 $('.btn_right').on('click', () => 
 {
-	$( ".slider_up li:first" ).appendTo( ".slider_up");
-	$( ".slider_down li:first" ).appendTo( ".slider_down");
+	var image_up = $( ".slider_up li:first" );
+	var image_down = $( ".slider_down li:first" );
+	var image_width= image_up.width();
+	image_up.detach().appendTo( ".slider_up");
+	image_down.detach().appendTo( ".slider_down");
+	$('.slider_up').css({
+		position: 'relative',
+		right: image_width
+	}).animate(
+		{
+			right: 0
+		}, 500
+	);
+	$('.slider_down').css({
+		position: 'relative',
+		right: image_width
+	}).animate(
+		{
+			right: 0
+		}, 500
+	);
 });
 $('.btn_left').on('click', () => 
 {
-	$( ".slider_up li:last" ).prependTo( ".slider_up");
-	$( ".slider_down li:last" ).prependTo( ".slider_down");
+	var image_up = $( ".slider_up li:last" );
+	var image_down = $( ".slider_down li:last" );
+	var image_width= image_up.width();
+	image_up.prependTo( ".slider_up");
+	image_down.detach().prependTo( ".slider_down");
+	$('.slider_up').css({
+		position: 'relative'
+	}).animate(
+		{
+			right: image_width
+		}, 500, () => {
+			$('.slider_up').css({right: 0})
+		});
+	$('.slider_down').css({
+		position: 'relative'
+	}).animate(
+		{
+			right: image_width
+		}, 500, () => {
+			$('.slider_down').css({right: 0})
+			});
 });
